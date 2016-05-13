468a5b440e328364b28c12f6cc6384b5602ba35b
Israel Pastor <ispaste@gmail.com>
2016-05-13 14:57:18 -0400

    more numbers and loops, now with comments

--- 02_printNumbers/main.go
+++ 02_printNumbers/main.go
@@ -3,6 +3,9 @@ package main
 import "fmt"
 
 func main()  {
+    // Print with format: %d = decimal, %b = binary, %x = hexadecimal, %o = octal, %q = UTF
+    // %X = hexadecimal in capital letters, %#x = complete 8-bit hexadecimal
+    // \t = tab, \n = new line
     fmt.Printf("%d - %b - %x - %o \n", 42, 42, 42, 42)
     fmt.Printf("%d \t %b \t %X \n", 42, 42, 42)
     fmt.Printf("%d \t %b \t %#x \n", 42, 42, 42)
--- 03_numberLoop/main.go
+++ 03_numberLoop/main.go
@@ -4,6 +4,6 @@ import "fmt"
 
 func main()  {
     for i := 0; i < 200; i++ {
-       fmt.Printf("%d \t\t %b \t\t %x \t\t %o \t\t %q \n", i, i, i, i, i) 
+        fmt.Printf("%d \t\t %b \t\t %x \t\t %o \t\t %q \n", i, i, i, i, i) 
     }
 }
